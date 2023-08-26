a1377d6448aa5627235d2476425c34e22c70562b
Larsluph <22910497+Larsluph@users.noreply.github.com>
2023-08-26 11:51:59 +0200

    Update jsl tags

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 function filterPhotosphere() {
-  const jsl_attrs = ["214", "941"];
-  const div_attrs = ["228", "955"];
+  const jsl_valid_attrs = ["214", "941"];
+  const div_valid_attrs = ["228", "955"];
 
   var oldCount = 1;
   var count = 0;
@@ -8,16 +8,15 @@ function filterPhotosphere() {
     oldCount = count;
     var jsl_tags = document.getElementsByTagName("jsl");
     for (let i = 0; i < jsl_tags.length; i++) {
-      // filter jsl tags with attibute: jstcache="214" || "941"
+      // filter jsl tags with valid jstcache
       var jsl = jsl_tags[i];
-      if (jsl_attrs.indexOf(jsl.getAttribute("jstcache")) == -1) continue;
-  
-      // get all div
+      if (jsl_valid_attrs.indexOf(jsl.getAttribute("jstcache")) == -1) continue;
+
       var divs = jsl.getElementsByTagName("div");
       for (let j = 0; j < divs.length; j++) {
-        //filter div tags with attribute: jstcache="228" || "955"
+        //filter div tags with valid jstcache
         var div = divs[j];
-        if (div_attrs.indexOf(div.getAttribute("jstcache")) == -1) continue;
+        if (div_valid_attrs.indexOf(div.getAttribute("jstcache")) == -1) continue;
 
         // if tag has style (display:none): delete it, not a 360° view
         if (div.hasAttribute("style")) {
